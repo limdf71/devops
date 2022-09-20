806a1ae1ae6c58735322ea2b61b982b011899a97
임재윤 <limdf71@gmail.com>
2022-09-20 10:12:10 +0900

    go1

--- Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
+++ Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "test-s3-tf-state" {
 resource "aws_dynamodb_table" "test-ddb-tf-lock" {
 
   depends_on   = [aws_s3_bucket.test-s3-tf-state]
-  name         = "skt-user00-test-s3-ddb-tflock"
+  name         = "skt-user11-test-s3-ddb-tflock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
