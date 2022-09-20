f92422a4845f81838c009f19aba77d1fe80ca632
임재윤 <limdf71@gmail.com>
2022-09-20 10:38:43 +0900

    go1`

--- Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
+++ Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "test-s3-tf-state" {
+resource "aws_s3_bucket" "skt-user11-test-s3-tf-state" {
 
   bucket = "skt-user11-test-s3-bucket-tfstate"
 
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "test-s3-tf-state" {
   
 }
 
-resource "aws_dynamodb_table" "test-ddb-tf-lock" {
+resource "aws_dynamodb_table" "skt-user11-test-ddb-tf-lock" {
 
   depends_on   = [aws_s3_bucket.test-s3-tf-state]
   name         = "skt-user11-test-s3-ddb-tflock"
