4dcccd08c7545af37e248ad5eeb0211df788372b
임재윤 <limdf71@gmail.com>
2022-09-20 10:40:31 +0900

    go1`

--- Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
+++ Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "skt-user11-test-s3-tf-state" {
+resource "aws_s3_bucket" "test-s3-tf-state" {
 
-  bucket = "skt-user11-test-s3-bucket-tfstate"
+  bucket = "test-s3-bucket-tfstate"
 
   tags = {
-    "Name" = "skt-user11-test-s3-bucket-tfstate"
+    "Name" = "test-s3-bucket-tfstate"
   }
   
 }
 
-resource "aws_dynamodb_table" "skt-user11-test-ddb-tf-lock" {
+resource "aws_dynamodb_table" "test-ddb-tf-lock" {
 
   depends_on   = [aws_s3_bucket.test-s3-tf-state]
-  name         = "skt-user11-test-s3-ddb-tflock"
+  name         = "test-s3-ddb-tflock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
@@ -21,7 +21,7 @@ resource "aws_dynamodb_table" "skt-user11-test-ddb-tf-lock" {
   }
 
   tags = {
-    "Name" = "skt-user11-test-s3-ddb-tflock"
+    "Name" = "test-s3-ddb-tflock"
   }
 
 }
